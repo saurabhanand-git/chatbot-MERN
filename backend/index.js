f2bed015099780fe3b9357671092b25e97b0d5eb
saurabhanand-git <saurabhk3001@gmail.com>
2023-05-12 06:35:16 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+app.use(bodyParser.json());
+app.use(cors());
+
+const configuration = new Configuration({
+  organization: "org-LCgGJS1OGQDv80bC7gyys9ho",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a airGPT. you can wait to give response.",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
